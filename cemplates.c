4be6a162bb336b6011aa9510ad3ec5f4b056ee21
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2016-01-28 17:30:17 +0100

    Possible implementation of sections using opaque pointers managed by the user

--- cemplates.c
+++ cemplates.c
@@ -2,15 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char string[] = "The capital of {{state}} is {{capital}}.\n";
+const char string[] = "Known capitals:\n\n{{#capitals}}  * Capital of {{state}} is {{city}}.\n{{/capitals}}\nEnd of list.\n";
 
-char * format(const char * key)
+/*
+ * the expected input object should by like:
+ *  { "capitals": [
+ *  	{"state": ..., "city": ...},
+ *  	{"state": ..., "city": ...}
+ *      ]
+ *  }
+ */
+
+struct mydata {
+	const char state[64];
+	const char city[64];
+};
+
+const struct mydata dataarray[] = {
+	{.state="Illinois", .city="Springfield"},
+	{.state="California", .city="Sacramento"},
+	{.state="Colorado", .city="Denver"},
+	{.state="Hawaii", .city="Honolulu"},
+};
+
+const void * format_section(const char * name, const void * previous)
+{
+	if (strcmp(name + 1, "capitals") == 0) {
+		struct mydata * pointer;
+		pointer = (struct mydata *) previous;
+		if (pointer == NULL)
+			return &dataarray[0];
+		if ((pointer - dataarray) + 1 >= sizeof(dataarray) / sizeof(struct mydata))
+			return NULL;
+		return pointer + 1;
+	} else
+		return NULL;
+};
+
+char * format_key(const char * key, const void * section)
 {
 	char * answer = NULL;
+	struct mydata * pointer;
+
+	pointer = (struct mydata *) section;
+
 	if (strcmp(key, "state") == 0)
-		answer = strdup("Illinois");
-	else if (strcmp(key, "capital") == 0)
-		answer = strdup("Springfield");
+		answer = strdup(pointer->state);
+	else if (strcmp(key, "city") == 0)
+		answer = strdup(pointer->city);
 	return answer;
 };
 
@@ -59,14 +98,26 @@ int main(int argc, char * argv[])
 {
 	const char *p;
 	char *key, *value;
+	const char * section_start;
+	const void * section;
 
+	section = NULL;
 	for (p = string; *p; p++) {
 		if (start_marker(p)) {
 			key = get_marker(&p);
-			value = format(key);
-			if (value != NULL) {
-				fwrite(value, 1, strlen(value), stdout);
-				free(value);
+			if (key[0] == '#') {
+				section = format_section(key, section);
+				section_start = p;
+			} else if (key[0] == '/') {
+				section = format_section(key, section);
+				if (section != NULL)
+					p = section_start;
+			} else {
+				value = format_key(key, section);
+				if (value != NULL) {
+					fwrite(value, 1, strlen(value), stdout);
+					free(value);
+				}
 			}
 			free(key);
 		} else {
